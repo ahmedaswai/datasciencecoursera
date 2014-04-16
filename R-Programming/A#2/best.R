3a49b09876705d05853a8ffeea5d2ed681ddd6f0
Bmind <ahmedaswai@gmail.com>
2014-04-17 01:51:22 +0300

    Adding R programming Assignments

--- R-Programming/A#2/best.R
+++ R-Programming/A#2/best.R
@@ -1,6 +1,35 @@
-best <- function(state, outcome) {
-  ## Read outcome data
-  ## Check that state and outcome are valid
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+best<-function( state,outcome )
+{
+  
+  outcomedata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  colbaseName<-"Hospital.30.Day.Death..Mortality..Rates.from."
+  stateDateFrame<-outcomedata[outcomedata$State==state,]
+  if(length(stateDateFrame$State)==0)
+  {
+    stop("invalid state")
+  }
+  if(outcome=="heart attack")
+  {
+    colbaseName<-paste(colbaseName,"Heart.Attack",sep="")
+  } 
+  else if(outcome=="heart failure")
+  { 
+    colbaseName<-paste(colbaseName,"Heart.Failure",sep="")
+  }   
+  
+  else if(outcome=="pneumonia")
+  {
+    colbaseName<-paste(colbaseName,"Pneumonia",sep="")
+  }
+  else
+  {
+    stop("invalid outcome")
+  }
+  
+  bestHosiptalNames<-stateDateFrame[suppressWarnings (as.numeric(  stateDateFrame[,colbaseName]))  ==min(suppressWarnings(as.numeric( stateDateFrame[,colbaseName]  ) ),na.rm=TRUE)
+                                    ,"Hospital.Name"]
+  
+   
+  bestHosiptalNames[5]
+ 
 }
