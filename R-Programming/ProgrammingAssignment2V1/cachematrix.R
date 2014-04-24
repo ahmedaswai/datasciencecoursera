74ee6038d003e5b803da9b45876c7696f8301a7e
Bmind <ahmedaswai@gmail.com>
2014-04-25 02:00:56 +0300

    Adding A2

---
+++ R-Programming/ProgrammingAssignment2V1/cachematrix.R
@@ -0,0 +1,18 @@
+
+# Function purpose: This function caches the matrix and find its cached inverse
+# Params:matrix
+#T Logic:The function is going to 
+# returns cached matrix 
+
+makeCacheMatrix <- function(x = matrix()) {
+  cache=List()
+  inverse<<-solve(x)
+  
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+    makeCacheMatrix(x)
+}
